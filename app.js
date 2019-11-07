ea2b547458ede645510e5b85bb2cab9a25192701
Muhammad Sehgal (Aquent LLC) <v-musehg@microsoft.com>
2019-11-06 17:19:21 -0800

    set up script to convert MD to HTML

--- app.js
+++ app.js
@@ -1,27 +1,6 @@
 var Request = require("request");
  
 const fs = require('fs');
-const doAsync = require('doasync');
-
-
-
-// // Start of markdown
-// var textToConvert = `Heading
-// =======
-// ## Sub-heading
- 
-// Paragraphs are separated
-// by a blank line.
- 
-// Two spaces at the end of a line  
-// produces a line break.
- 
-// Text attributes _italic_, 
-// **bold**, 'monospace'.
-// A [link](http://example.com).
-// Horizontal rule:`;
- 
-// // End of markdown
                   
 var data = fs.readFileSync("./articles/privateAtlas.md", { "encoding": "utf8"});
 
