db9d2ec3e59a3171786278896c08271beb119d8f
Muhammad Sehgal (Aquent LLC) <v-musehg@microsoft.com>
2019-11-06 17:18:16 -0800

    set up script to convert MD to HTML

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const showdown   = require('showdown');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+converter = new showdown.Converter();
+
+app.post("/convert", function(req, res, next) {
+    if(typeof req.body.content == 'undefined' || req.body.content == null) {
+        res.json(["error", "No data found"]);
+    } else {
+        text = req.body.content;
+        html = converter.makeHtml(text);
+        res.json(["markdown", html]);
+    }
+});
+ 
+app.listen(3000, function() {
+    console.log("Server running on port 3000");
+   });
